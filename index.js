11fca7258429311b5a94e5c558832ebf83a3b491
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-06-30 14:56:51 +0100

    inquirer foundation

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
 const Manager = require ('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require ('./lib/Intern');
+
+let managerAccount;
+let enginnerAccount;
+let internAccount;
+
+questions()
+
+function questions(){
+    const inquirer = require ('inquirer');
+    inquirer
+    .prompt([
+        {
+            type:"list",
+            name:"positionsList",
+            message:"Pick a position",
+            choices:["Manager", "Enginner", "Intern"]
+        },
+    ])
+    .then((position) => {
+        if (position.positionsList == "Manager"){
+            managerQuestions (position)
+        }
+
+        if (position.positionsList == "Enginner"){
+            engineerQuestions (position)
+        }
+
+        if (position.positionsList == "Intern"){
+            internQuestions (position)
+        }
+    })
+
+    let managerQuestions = () =>{
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                name:"name",
+                message:"Enter your name.",
+            },
+            {
+                type: "input",
+                name:"id",
+                message:"Enter your id.",
+            },
+            {
+                type: "input",
+                name:"email",
+                message:"Enter your email.",
+            },
+            {
+                type: "input",
+                name:"officeNumber",
+                message:"Enter your office number.",
+            },
+        ])
+        .then ((response)=>{
+            managerAccount = new Manager (response.name, response.id, response.email, response.officeNumber)
+            managerHtml()
+            questions.call(this)
+        })
+    }
+}
